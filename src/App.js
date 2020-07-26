a65d16ad94109089c108e51da2019a012e3fac75
Manolingam N <manolingam1996@gmail.com>
2020-07-26 18:38:00 +0530

    removed unused Link import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
